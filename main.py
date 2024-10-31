56ec492c8a5455f42112af84b78e4804effbc5b4
Nikodem Bochnacki <nikodem.bochnacki@iaisa.com>
2024-10-31 19:06:01 +0100

    added countWords() to print am. of words

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ def openAndPrint():
         file_contents = f.read()
         print(file_contents)
 
+def countWords():
+    with open("books/frankenstein.txt") as f:
+        contents = f.read()
+        words = contents.split()
+        counter = 0
+        for word in words:
+            counter += 1
+        return print(f"The book has {counter} words.")
+
 def main():
     openAndPrint()
+    countWords()
 
 main()
