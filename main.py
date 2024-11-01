f159119d2404154ca2924e2b096306b167ea4ad4
Nikodem Bochnacki <nikodem.bochnacki@iaisa.com>
2024-11-01 15:36:31 +0100

    add countChars(text)

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-def openAndPrint():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-
-def countWords():
-    with open("books/frankenstein.txt") as f:
-        contents = f.read()
-        words = contents.split()
-        counter = 0
-        for word in words:
-            counter += 1
-        return print(f"The book has {counter} words.")
-
 def main():
-    openAndPrint()
-    countWords()
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    print(text)
+    countWords(text)
+    countChars(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def countWords(text):
+    words = text.split()
+    counter = 0
+    for word in words:
+        counter += 1
+    return print(f"The book has {counter} words.")
+    
+def countChars(text):
+    lowered_string = text.lower()
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    chars = {char: 0 for char in letters}
+    
+    for char in lowered_string:
+        if char in chars:
+            chars[char] += 1
+    return print(f"The book has characters: {chars}.")
 
 main()
